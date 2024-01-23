79f3bd735fdefbb0f4a5f6cfe890325475ec5130
149036 <96453230+149036@users.noreply.github.com>
2024-01-24 05:43:28 +0900

    Update setup_arch.sh

--- setup_arch.sh
+++ setup_arch.sh
@@ -2,12 +2,37 @@
 
 yes | sudo pacman -S yay
 yes | yay -S neovim-nightly-bin \
-    zsh \
+    fzf\
+    zsh\
     starship\
-    docker \
+    docker\
     docker-compose\
     nodejs\
-    npm
+    npm\
+    eza\
+    go\
+    ghq\
+    peco\
+
+
+mkdir ~/repo
+git config --global ghq.root '~/repo'
+ghq get https://github.com/marlonrichert/zsh-autocomplete.git
+
+
+
+
+
+
+
+## font HackGen Nerd Fonts Console
+wget https://github.com/yuru7/HackGen/releases/download/v2.9.0/HackGen_NF_v2.9.0.zip
+unzip HackGen_NF_v2.9.0.zip
+cp -r HackGen_NF_v2.9.0 ~/.local/share/fonts
+fc-cache -vf
+rm -rf HackGen_NF_v2.9.0 HackGen_NF_v2.9.0.zip
+
+
 
 sudo systemctl enable docker\
     sshd
@@ -44,13 +69,61 @@ touch ~/.zshrc
 # .zshrc
 ################################################################################################################
 cat << EOF > ~/.zshrc
-lias l="ls -l"
+# zsh-autocomplete settings
+
+source ~/repo/github.com/marlonrichert/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+alias l="ls -l"
 alias la="ll -a"
 alias vim="nvim"
 
 
-###
+# peco settings
+## 過去に実行したコマンドを選択。ctrl-rにバインド。
+function peco-select-history() {
+  BUFFER=$(\history -n -r 1 | peco --query "$LBUFFER")
+  CURSOR=$#BUFFER
+  zle clear-screen
+}
+zle -N peco-select-history
+bindkey '^R' peco-select-history
+
+## 過去に実行したディレクトリ移動を選択。ctrl-gにバインド。
+if [[ -n $(echo ${^fpath}/chpwd_recent_dirs(N)) && -n $(echo ${^fpath}/cdr(N)) ]]; then
+    autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+    add-zsh-hook chpwd chpwd_recent_dirs
+    zstyle ':completion:*' recent-dirs-insert both
+    zstyle ':chpwd:*' recent-dirs-default true
+    zstyle ':chpwd:*' recent-dirs-max 1000
+    zstyle ':chpwd:*' recent-dirs-file "$HOME/.cache/chpwd-recent-dirs"
+fi
+
+function peco-cdr () {
+  local selected_dir="$(cdr -l | sed 's/^[0-9]\+ \+//' | peco --prompt="cdr >" --query "$LBUFFER")"
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd `echo $selected_dir | awk '{print$2}'`"
+    CURSOR=$#BUFFER
+    zle reset-prompt
+  fi
+}
+zle -N peco-cdr
+bindkey '^G' peco-cdr
+
+## ghqとの連携。ghqの管理化にあるリポジトリを一覧表示する。ctrl - ]にバインド。
+function peco-repo () {
+  local selected_dir=$(ghq list -p | peco --prompt="repo >" --query "$LBUFFER")
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd ${selected_dir}"
+    zle accept-line
+  fi
+  zle clear-screen
+}
+zle -N peco-repo
+bindkey '^]' peco-repo
+
+
+### 必ず末尾にかく starship
 eval "$(starship init zsh)"
+
 EOF
 ################################################################################################################
 
